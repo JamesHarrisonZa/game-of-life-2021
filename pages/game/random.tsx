ab6695c886ac1ddb10bc166d8da4cb42c218d677
JamesH <James.Harrison@serko.com>
2021-11-16 15:33:48 +1300

    Clean up index a bit and added random page

---
+++ pages/game/random.tsx
@@ -0,0 +1,5 @@
+const GameRandom: React.FC = () => {
+  return <h1>Will randomise the grid</h1>;
+};
+
+export default GameRandom;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
-import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import utilStyles from '../styles/utils.module.css';
 import FormattedDate from '../components/FormattedDate';
 import Layout, { siteTitle } from '../components/layout';
 
-export default function Home({
-  allPostsData,
-}: {
-  allPostsData: {
-    date: string;
-    title: string;
-    id: string;
-  }[];
-}) {
+const Home: React.FC = () => {
   const todayDate = new Date();
 
   return (
@@ -21,24 +12,33 @@ export default function Home({
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
+      <small className={utilStyles.lightText}>
+        <FormattedDate date={todayDate} />
+      </small>
+
       <section className={utilStyles.headingMd}>
         <p>Hi! </p>
         <p>Bla bla bla</p>
       </section>
+
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Starting grid</h2>
         <ul className={utilStyles.list}>
           <li className={utilStyles.listItem} key={1}>
             <Link href={`/game/forty-two`}>
-              <a>{42}</a>
+              <a>{'42'}</a>
             </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <FormattedDate date={todayDate} />
-            </small>
+          </li>
+          <li className={utilStyles.listItem} key={2}>
+            <Link href={`/game/random`}>
+              <a>{'Random'}</a>
+            </Link>
           </li>
         </ul>
       </section>
     </Layout>
   );
-}
+};
+
+export default Home;
