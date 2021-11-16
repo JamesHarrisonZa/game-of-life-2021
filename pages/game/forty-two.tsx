c017c2558a241bc3b2f3f48366e4b52d6b0b3e47
JamesH <James.Harrison@serko.com>
2021-11-16 15:41:33 +1300

    Add links back to home

--- pages/game/forty-two.tsx
+++ pages/game/forty-two.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+
 const GameFortyTwo: React.FC = () => {
-  return <h1>42</h1>;
+  return (
+    <>
+      <h1>42</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 };
 
 export default GameFortyTwo;
--- pages/game/random.tsx
+++ pages/game/random.tsx
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+
 const GameRandom: React.FC = () => {
-  return <h1>Will randomise the grid</h1>;
+  return (
+    <>
+      <h1>Will randomise the grid</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
 };
 
 export default GameRandom;
